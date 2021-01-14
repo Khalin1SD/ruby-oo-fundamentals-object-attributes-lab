658b61c67931045784a344e5f1e8d9ffc23213f5
Khalin Redding <khalinredd@gmail.com>
2021-01-14 16:21:07 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+    attr_accessor :name, :breed
+    
+       def initialize
+           @job = job
+       end 
+   
+       def job
+           @job
+       end
+   
+       def job=(job)
+           @job = job 
+       end
+   
+   
+   
+   end
